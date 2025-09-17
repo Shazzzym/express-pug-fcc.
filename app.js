4d2288ae5cb796873d1de3264b3a91d30a986775
Shazzzym <solani.mpho@gmail.com>
2025-09-18 00:46:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-// Set methods AFTER the route (as suggested in forum)
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
+app.route('/').get((req, res) => {
+  res.render('index');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log('Listening on port ' + port);
 });
