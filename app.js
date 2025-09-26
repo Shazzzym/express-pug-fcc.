2ab68f82fd6c5509490d21c734f3a6bfdfb16853
Shazzzym <solani.mpho@gmail.com>
2025-09-26 22:22:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
+  // Change the response to render the Pug template
   res.render('index');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
